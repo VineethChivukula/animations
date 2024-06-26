0fe40b08106dfbe323f1510cbade701773a87066
VineethChivukula <vineethonsocialmedia@gmail.com>
2024-06-26 19:49:23 +0530

    commit

--- TwoSumII/Solution.java
+++ TwoSumII/Solution.java
@@ -2,26 +2,26 @@
 // SC: O(1)
 
 public class Solution {
-	public int[] twoSum(int[] numbers, int target) {
-		int i = 0, j = numbers.length - 1;
+	public int[] twoSum(int[] nums, int target) {
+		int i = 0, j = nums.length - 1;
 		while (i < j) {
-			int sum = numbers[i] + numbers[j];
+			int sum = nums[i] + nums[j];
 			if (sum == target) {
-				return new int[]{i + 1, j + 1}; // Found a pair that adds up to the target
+				return new int[] { i + 1, j + 1 }; // Found a pair that adds up to the target
 			} else if (sum < target) {
 				i++; // Increment i to try a larger number from the left side
 			} else {
 				j--; // Decrement j to try a smaller number from the right side
 			}
 		}
-		return new int[]{-1, -1}; // No pair found that adds up to the target
+		return new int[] { -1, -1 }; // No pair found that adds up to the target
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		int[] numbers = {2, 7, 11, 15};
+		int[] nums = { 2, 7, 11, 15 };
 		int target = 18;
-		int[] result = sol.twoSum(numbers, target);
+		int[] result = sol.twoSum(nums, target);
 		System.out.println(result[0] + " " + result[1]); // Print the indices of the pair
 	}
 }
