7fad9d27196b3ac8c2a9400d57f28ae594458851
VineethChivukula <vineethonsocialmedia@gmail.com>
2024-07-10 18:10:06 +0530

    commit

---
+++ ReverseLinkedList/Solution.java
@@ -0,0 +1,42 @@
+// TC: O(n)
+// SC: O(1)
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head, next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Solution rl = new Solution();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode res = rl.reverseList(head);
+        while (res != null) {
+            System.out.print(res.val + " ");
+            res = res.next;
+        }
+    }
+}
